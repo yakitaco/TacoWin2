8bb52907627a2b2aa7551ef7cade11631e5b1775
YAKI TACO <tnknet21@gmail.com>
2021-12-20 23:57:01 +0900

    [TacoWin2]Add base of the standard moves library.

---
+++ TacoWin2/TacoWin2/tw2stmove.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TacoWin2 {
+    class tw2stmove {
+        public static kmove rootKmv;  //大元の定跡リスト
+        public static kmove currentKmv;  //現在の定跡リスト
+        static Random rnd = new Random();
+
+        public static int readFile(string filePath) {
+            int ret = -1;
+            rootKmv = kmove.load(filePath);
+            if (rootKmv != null) ret = 0;
+            return ret;
+        }
+
+        // 定跡リストを取得
+        public static List<koPos> getSmove(BanInfo ban, int teban) {
+            BanInfo tmpBan = new BanInfo(ban);
+            List<koPos> retList = new List<koPos>();
+            kmove tmpKmv = currentKmv;
+
+            for (int cnt = 0; (tmpKmv != null) && (tmpKmv.nxSum > 0) && (cnt < 20); cnt++) {
+                int rVal = rnd.Next(0, tmpKmv.nxSum);
+                for (int i = 0; i < tmpKmv.nxMove.Count; i++) {
+                    if (tmpKmv.nxMove[i].val < 0) continue;
+                    if (rVal > tmpKmv.nxMove[i].val) {
+                        rVal -= tmpKmv.nxMove[i].val;
+                        continue;
+                    }
+                    Form1.Form1Instance.addMsg("Hit Move : (" + tmpKmv.nxMove[i].ox + "," + tmpKmv.nxMove[i].oy + ")->(" + tmpKmv.nxMove[i].nx + "," + tmpKmv.nxMove[i].ny + "):" + tmpKmv.nxMove[i].val);
+
+                    // 移動チェック
+                    int ret;
+                    koPos moveKoma;
+
+                    if (tmpKmv.nxMove[i].ox == 9) { // 駒打ち
+                        moveKoma = new koPos(tmpKmv.nxMove[i].nx, tmpKmv.nxMove[i].ny);
+                        moveKoma.ko = new koma(tmpBan.MochiKo[teban, tmpKmv.nxMove[i].oy - 1][0]);
+                        ret = tmpBan.moveKoma(teban, moveKoma.ko.type, moveKoma, true);
+                    } else { // 移動
+                        koPos src = new koPos(tmpKmv.nxMove[i].ox, tmpKmv.nxMove[i].oy);
+                        moveKoma = new koPos(tmpKmv.nxMove[i].nx, tmpKmv.nxMove[i].ny);
+                        moveKoma.ko = new koma(tmpBan.BanKo[tmpKmv.nxMove[i].ox, tmpKmv.nxMove[i].oy]);
+                        ret = tmpBan.moveKoma(src, moveKoma, tmpKmv.nxMove[i].nari, true);
+                    }
+
+                    if (ret == 0) {  //移動OK
+                                     // 移動リストに追加
+                        retList.Add(moveKoma);
+                        tmpKmv = tmpKmv.nxMove[i];
+                    } else {  //移動NG
+                        Form1.Form1Instance.addMsg("Move chk NG!!");
+                        break;
+                    }
+                    break;
+                }
+                teban = (teban == TEIGI.TEBAN_SENTE ? TEIGI.TEBAN_GOTE : TEIGI.TEBAN_SENTE);
+            }
+
+            return retList;
+        }
+    }
+}
---
+++ TacoWin2/TacoWin2_SMV/sMove.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace TacoWin2_SMV {
+    public class sMove {
+        public static string[,] sList = new string[500,500];
+        public int sListNum = 0;
+
+        //ファイルから読み取り
+        public static void load() {
+            //開くファイルを選択するダイアログを開く
+            kmove loadedData = null;
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "YTSG定跡テキスト(*.ytx)|*.ytx";
+            ofd.InitialDirectory = Directory.GetCurrentDirectory() + @"\Userdata";
+            if (ofd.ShowDialog() == DialogResult.OK) {
+                //ファイルを読込
+                Stream fileStream = ofd.OpenFile();
+                BinaryFormatter binaryFormatter = new BinaryFormatter();
+                loadedData = (kmove)binaryFormatter.Deserialize(fileStream);
+                fileStream.Close();
+            }
+        }
+
+        public static void load(string filePath) {
+            //開くファイルを選択するダイアログを開く
+            kmove loadedData = null;
+            if (System.IO.File.Exists(filePath)) {
+                //ファイルを読込
+                Stream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+                BinaryFormatter binaryFormatter = new BinaryFormatter();
+                loadedData = (kmove)binaryFormatter.Deserialize(fileStream);
+                fileStream.Close();
+            } else {
+                //MessageBox.Show("'" + filePath + "'は存在しません。");
+            }
+        }
+
+        //ファイルへセーブ
+        public bool save() {
+            //保存先を指定するダイアログを開く
+            System.IO.Directory.CreateDirectory(@"Userdata");
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "YTSG定跡データ(*.ytj)|*.ytj";
+            sfd.InitialDirectory = Directory.GetCurrentDirectory() + @"\Userdata";
+            sfd.FileName = "dat" + DateTime.Now.Year + DateTime.Now.Month + DateTime.Now.Day + "_" + DateTime.Now.Hour + DateTime.Now.Minute + DateTime.Now.Second;
+            if (sfd.ShowDialog() == DialogResult.OK) {
+                //指定したパスにファイルを保存する
+                Stream fileStream = sfd.OpenFile();
+                BinaryFormatter bF = new BinaryFormatter();
+                bF.Serialize(fileStream, this);
+                fileStream.Close();
+            }
+            return true;
+        }
+
+        public bool save(string filePath) {
+            Stream fileStream = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            binaryFormatter.Serialize(fileStream, this);
+            fileStream.Close();
+            return true;
+        }
+
+
+
+
+
+    }
+}
