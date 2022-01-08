8b01ffa45f3a2e8bfe30c858065a0a0e990b9ab3
YAKI TACO <tnknet21@gmail.com>
2022-01-08 23:58:31 +0900

    [TacoWin2]Add debug form.

---
+++ TacoWin2/TacoWin2/DebugForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TacoWin2 {
+    public partial class DebugForm : Form {
+
+        private static DebugForm _formInstance;
+
+        public DebugForm() {
+            InitializeComponent();
+            _formInstance = this;
+        }
+
+        //Form1オブジェクトを取得、設定するためのプロパティ
+        public static DebugForm instance {
+            get {
+                return _formInstance;
+            }
+            set {
+                _formInstance = value;
+            }
+        }
+
+        delegate void delegate1(String text1);
+        delegate void delegate2();
+
+        public void addMsg(string msg) {
+            Invoke(new delegate1(_addMsg), msg);
+        }
+
+        public void _addMsg(string msg) {
+            textBox1.AppendText(msg + "\r\n");
+            System.Diagnostics.Debug.WriteLine(msg);
+        }
+
+        public void resetMsg() {
+            Invoke(new delegate2(_resetMsg));
+        }
+
+        public void _resetMsg() {
+            textBox1.ResetText();
+        }
+
+        public string getText() {
+            return textBox1.Text;
+        }
+
+    }
+}
---
+++ TacoWin2/TacoWin2/tw2_log.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace TacoWin2 {
+    class tw2_log {
+        public static void save(string str, int teban) {
+            DateTime dt = DateTime.Now;
+            string result = dt.ToString("yyyyMMdd_HHmmss");
+            if (!Directory.Exists("log")) {
+                Directory.CreateDirectory("log");
+            }
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance); // memo: Shift-JISを扱うためのおまじない
+            StreamWriter sw = new StreamWriter("log/" + result + "_" + teban + ".txt", false, Encoding.GetEncoding("shift_jis"));
+            sw.Write(str);
+            sw.Close();
+
+        }
+
+    }
+}
