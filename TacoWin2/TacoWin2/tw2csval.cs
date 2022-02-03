2e87c868fd16ba0c540bc5abd1b5a7c211488f9e
YAKI TACO <yaki_taco@yahoo.co.jp>
2022-02-03 22:56:20 +0900

    [TacoWin2]Add castle list.

---
+++ TacoWin2/TacoWin2/tw2csval.cs
@@ -0,0 +1,58 @@
+﻿namespace TacoWin2 {
+    class tw2csval {
+        public enum STLIST : int {
+            None = 0,
+            YAGURA = 100,   //(金)矢倉
+            GINYAGURA = 110,//銀矢倉
+            KATAYAGURA = 120,//片矢倉
+            SOUYAGURA = 130,//総矢倉
+            KIKUSUIYAGURA = 140,//菊水矢倉
+            GINDACHIYAGURA = 150,//銀立ち矢倉
+            HISHIYAGURA = 160,//菱矢倉
+            FUJIMIYAGURA = 170,//富士見矢倉
+            YAGURAANAGUMA = 180,//矢倉穴熊
+            HEKOMIYAGURA = 190,//へこみ矢倉
+            NAGAREYAGURA = 200,//流れ矢倉
+            RYUUSENYAGURA = 210,//流線矢倉
+            KAKUYAGURA = 220,//角矢倉
+
+            HIDARIMINO = 400,//左美濃囲い
+            YONMAIMINO = 410,//四枚美濃
+            TENSYUKAKUMINO = 420,//天守閣美濃
+            IBISYAANAGUMA = 430,//居飛車穴熊
+            FUNAGAKOI = 440,//舟囲い
+            GANGIGAKOI = 450,//雁木囲い
+            MILLENNIUMGAKOI = 460,//ミレニアム囲い
+            KUSHIKATUKAKOI = 470,//串カツ囲い
+            NAKAZUMAI = 480,//中住まい
+            NAKAHARAGAKOI = 490,//中原囲い
+            KANIGAKOI = 500,//カニ囲い
+            BONANZAGAKOI = 510,//ボナンザ囲い
+            CYUDANGYOKU = 520,//中段玉
+            SAIKYOUGAKOI = 530,//最強囲い
+            ELMOGAKOI = 540,//elmo囲い
+
+
+            MINOGAKOI = 700,//美濃
+            TAKAMINO = 710,//高美濃
+            GINKANMURI = 720,//銀冠
+            GINMINO = 730,//銀美濃
+            DIAMONDMINO = 740,//ダイヤモンド美濃
+            KIMURAMINO = 750,//木村美濃
+            KATAMINO = 760,//片美濃
+            CYONMAGEMINO = 770,//ちょんまげ美濃
+            BOUZUMINO = 780,//坊主美濃
+
+            FURIANAGUMA = 800,//振り飛車穴熊
+            GINKANNANA = 810,//銀冠穴熊
+            BIGFOUR = 820,//ビッグ４
+
+            MIGIYAGURA = 900,//右矢倉
+            KINMUSOU = 910,//金無双
+            HAYAKAKOI = 920,//早囲い
+            SANTEKAKOI = 930,//三手囲い
+            TADENKAKOI = 940,//多伝囲い
+
+        }
+    }
+}
